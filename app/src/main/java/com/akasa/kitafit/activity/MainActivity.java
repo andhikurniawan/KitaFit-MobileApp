81c99b38c1336f3673743e6ccf3186af32d5018e
Adhitya Kusumadinatha <adhityasantika@yahoo.com>
2020-08-21 22:19:50 +0800

    Done : Lini Masa Frargment layout. Add : Model (not completed) and adapter

--- app/src/main/java/com/akasa/kitafit/activity/MainActivity.java
+++ app/src/main/java/com/akasa/kitafit/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.akasa.kitafit;
+package com.akasa.kitafit.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.akasa.kitafit.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/akasa/kitafit/model/LiniMasaData.java
@@ -0,0 +1,4 @@
+package com.akasa.kitafit.model;
+
+public class LiniMasaData {
+}
