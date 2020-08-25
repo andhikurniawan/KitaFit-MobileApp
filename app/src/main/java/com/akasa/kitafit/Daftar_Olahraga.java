5b7b953874b49e5a26c1cf37925fbe154249f75c
Syfanur <j0h@example.com>
2020-08-25 19:28:34 +0700

    beranda, daftar dan detail olahraga, aktivitas

---
+++ app/src/main/java/com/akasa/kitafit/Daftar_Olahraga.java
@@ -0,0 +1,14 @@
+package com.akasa.kitafit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Daftar_Olahraga extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daftar__olahraga);
+    }
+}
