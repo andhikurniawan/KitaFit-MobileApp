fa4e5f53145df5c4423d7fa55a6f30db0de9c3a2
Adhitya Kusumadinatha <adhityasantika@yahoo.com>
2020-08-31 12:34:34 +0800

    Commit before pull branch andhi

--- app/src/main/java/com/akasa/kitafit/activity/PostLiniMasa.java
+++ app/src/main/java/com/akasa/kitafit/activity/PostLiniMasa.java
@@ -1,5 +1,6 @@
 package com.akasa.kitafit.activity;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,15 +8,36 @@ import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.akasa.kitafit.R;
+import com.akasa.kitafit.model.LiniMasaData;
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class PostLiniMasa extends AppCompatActivity {
     private static final String TAG = "PostLiniMasa";
@@ -25,6 +47,14 @@ public class PostLiniMasa extends AppCompatActivity {
     EditText captionField;
     Button postButton;
     ProgressBar progressBar;
+    DatabaseReference ref, counterRef, imageRef, userRef;
+    StorageTask mUploadTask;
+    StorageReference mStorageRef;
+    int idPost = 0;
+    String nama_user, foto_user, tanggal_post, foto_post, caption_post;
+    int jumlah_like = 0;
+    Locale locale = new Locale("id", "ID");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +65,21 @@ public class PostLiniMasa extends AppCompatActivity {
         captionField = findViewById(R.id.caption_post_linimasa_field);
         postButton = findViewById(R.id.button_post_linimasa);
         progressBar = findViewById(R.id.horizontalProgressBar);
+        ref = FirebaseDatabase.getInstance().getReference("lini_masa");
+        counterRef = FirebaseDatabase.getInstance().getReference("lini_masa").child("counter_id_post");
+        userRef = FirebaseDatabase.getInstance().getReference("user").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+        // cek counter post + user data
+        cekCounterPost();
+        getUserData();
+
+        // cek hari ini
+        Date c = Calendar.getInstance().getTime();
+        System.out.println("Current time : "+c);
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("kk.mm, dd MMMM yyyy", locale);
+        String formateDate = simpleDateFormat.format(c);
+        Log.d(TAG, "Hari ini : "+formateDate);
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,7 +99,93 @@ public class PostLiniMasa extends AppCompatActivity {
         postButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Post Clicked
+                if (mImageUri == null){
+                    Toast.makeText(PostLiniMasa.this, "Silakan Pilih Gambar Terlebih Dahulu", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (mUploadTask != null && mUploadTask.isInProgress()){
+                    Toast.makeText(PostLiniMasa.this, "Proses Upload sedang berlangsung, mohon bersabar.", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (mImageUri != null){
+                        mStorageRef = FirebaseStorage.getInstance().getReference("liniMasaPost").child(String.valueOf(idPost));
+                        StorageReference fileReference = mStorageRef.child(String.valueOf(idPost)+"_"+System.currentTimeMillis()+"."+getFileExtension(mImageUri));
+                        mUploadTask = fileReference.putFile(mImageUri)
+                                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                    @Override
+                                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                        Handler handler = new Handler();
+                                        handler.postDelayed(new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                progressBar.setProgress(0);
+                                            }
+                                        }, 2000);
+                                        Task<Uri> result = taskSnapshot.getStorage().getDownloadUrl();
+                                        result.addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                            @Override
+                                            public void onSuccess(Uri uri) {
+                                                foto_post = uri.toString();
+                                                idPost++;
+                                                counterRef.setValue(idPost);
+                                                LiniMasaData liniMasaData = new LiniMasaData(idPost, nama_user, foto_user, tanggal_post, foto_post, caption_post, jumlah_like);
+                                            }
+                                        });
+                                    }
+                                });
+                    } else {
+                        Toast.makeText(PostLiniMasa.this, "Anda Belum Memilih Gambar", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+
+    private void getUserData() {
+        Log.d(TAG, "getUserData: getting User Data..");
+        // get user name
+        userRef.child("nama_user").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                nama_user = snapshot.getValue().toString();
+                Log.d(TAG, "onDataChange-UserName: "+nama_user);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        
+        // get foto user
+        userRef.child("foto_user").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                foto_user = snapshot.getValue().toString();
+                Log.d(TAG, "onDataChange-FotoUser: "+foto_user);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void cekCounterPost() {
+        Log.d(TAG, "cekCounterPost: Checking Counter...");
+        counterRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    idPost = Integer.parseInt(snapshot.getValue().toString());
+                } else {
+                    counterRef.setValue(idPost);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
     }
@@ -71,7 +202,7 @@ public class PostLiniMasa extends AppCompatActivity {
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
             mImageUri = data.getData();
 
-            Glide.with(this).load(mImageUri).into(fotoPost);
+            Glide.with(this).load(mImageUri).centerCrop().into(fotoPost);
         }
     }
     private String getFileExtension(Uri uri) {
--- app/src/main/java/com/akasa/kitafit/model/LiniMasaData.java
+++ app/src/main/java/com/akasa/kitafit/model/LiniMasaData.java
@@ -1,4 +1,77 @@
 package com.akasa.kitafit.model;
 
 public class LiniMasaData {
+    private int id_post;
+    private String nama_user;
+    private String foto_user;
+    private String tanggal_post;
+    private String foto_post;
+    private String caption_post;
+    private int jumlah_like;
+
+    public LiniMasaData(int id_post, String nama_user, String foto_user, String tanggal_post, String foto_post, String caption_post, int jumlah_like) {
+        this.id_post = id_post;
+        this.nama_user = nama_user;
+        this.foto_user = foto_user;
+        this.tanggal_post = tanggal_post;
+        this.foto_post = foto_post;
+        this.caption_post = caption_post;
+        this.jumlah_like = jumlah_like;
+    }
+
+    public int getId_post() {
+        return id_post;
+    }
+
+    public void setId_post(int id_post) {
+        this.id_post = id_post;
+    }
+
+    public String getNama_user() {
+        return nama_user;
+    }
+
+    public void setNama_user(String nama_user) {
+        this.nama_user = nama_user;
+    }
+
+    public String getFoto_user() {
+        return foto_user;
+    }
+
+    public void setFoto_user(String foto_user) {
+        this.foto_user = foto_user;
+    }
+
+    public String getTanggal_post() {
+        return tanggal_post;
+    }
+
+    public void setTanggal_post(String tanggal_post) {
+        this.tanggal_post = tanggal_post;
+    }
+
+    public String getFoto_post() {
+        return foto_post;
+    }
+
+    public void setFoto_post(String foto_post) {
+        this.foto_post = foto_post;
+    }
+
+    public String getCaption_post() {
+        return caption_post;
+    }
+
+    public void setCaption_post(String caption_post) {
+        this.caption_post = caption_post;
+    }
+
+    public int getJumlah_like() {
+        return jumlah_like;
+    }
+
+    public void setJumlah_like(int jumlah_like) {
+        this.jumlah_like = jumlah_like;
+    }
 }
