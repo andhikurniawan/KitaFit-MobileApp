338d992c3e446d6434e1167d0293e42e0d3a3a21
andhikurniawan19 <andhiikurniawann@gmail.com>
2020-09-05 11:43:40 +0700

    history

--- app/src/main/java/com/akasa/kitafit/activity/Homepage.java
+++ app/src/main/java/com/akasa/kitafit/activity/Homepage.java
@@ -22,13 +22,17 @@ import com.akasa.kitafit.activity.OlahragaViewHolder;
 import com.akasa.kitafit.activity.ProgramViewHolder;
 import com.akasa.kitafit.model.OlahragaItem;
 import com.akasa.kitafit.model.ProgramItem;
+import com.akasa.kitafit.model.usermodel;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.mikhaellopez.circularimageview.CircularImageView;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
@@ -37,13 +41,16 @@ public class Homepage extends AppCompatActivity {
     LinearLayoutManager mLayoutManager;
     SharedPreferences mSharedPref;
     EditText inputSearch;
+    private FirebaseUser user;
+    private FirebaseDatabase firebaseDatabase;
+    String UID;
     RecyclerView recyclerView;
     FirebaseRecyclerOptions<OlahragaItem> options;
     FirebaseRecyclerAdapter<OlahragaItem, OlahragaViewHolder> adapter;
     FirebaseRecyclerOptions<ProgramItem> optionss;
     FirebaseRecyclerAdapter<ProgramItem, ProgramViewHolder> adapterr;
     DatabaseReference databaseReference;
-
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,12 +60,30 @@ public class Homepage extends AppCompatActivity {
         program();
 
 
-        TextView Nama = findViewById(R.id.namauser);
-        TextView Umur = findViewById(R.id.umur);
-        ImageView Foto = findViewById(R.id.user);
+        final TextView Nama = findViewById(R.id.namauser);
+        final TextView Umur = findViewById(R.id.umur);
+        final CircularImageView profil = findViewById(R.id.user);
 
-        infodisplay(Nama, Umur);
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        UID=user.getUid();
 
+//        infodisplay(Nama, Umur);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = firebaseDatabase.getReference("user");
+        databaseReference.child(UID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                usermodel user = dataSnapshot.getValue(usermodel.class);
+                Nama.setText(user.getNama_user());
+                Picasso.get().load(user.getFoto_user()).into(profil);
+                Umur.setText(user.getUmur());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(Homepage.this, databaseError.getCode(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
@@ -169,36 +194,41 @@ public class Homepage extends AppCompatActivity {
 
     }
 
-
-    private void infodisplay(final TextView Nama, final TextView Umur) {
-        DatabaseReference UsersRef = FirebaseDatabase.getInstance().getReference().child("user").child("1jVxNEtqxTSQpxK7NDFLn68NNrl2");
-        UsersRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot)
-            {
-                if (dataSnapshot.exists())
-                {
-                    if (dataSnapshot.child("email").exists())
-                    {
-                        String umur = dataSnapshot.child("umur").getValue().toString();
-                        String nama = dataSnapshot.child("nama_user").getValue().toString();
-
-                        Umur.setText(umur);
-                        Nama.setText(nama);
-
-                    }
-                }else{
-
-                }
-            }
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-            }
-        });
-    }
+//
+//    private void infodisplay(final TextView Nama, final TextView Umur) {
+//        DatabaseReference UsersRef = FirebaseDatabase.getInstance().getReference().child("user").child("1jVxNEtqxTSQpxK7NDFLn68NNrl2");
+//        UsersRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot)
+//            {
+//                if (dataSnapshot.exists())
+//                {
+//                    if (dataSnapshot.child("email").exists())
+//                    {
+//                        String umur = dataSnapshot.child("umur").getValue().toString();
+//                        String nama = dataSnapshot.child("nama_user").getValue().toString();
+//
+//                        Umur.setText(umur);
+//                        Nama.setText(nama);
+//
+//                    }
+//                }else{
+//
+//                }
+//            }
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//            }
+//        });
+//    }
 
     public void olahraga(View view) {
         Intent b = new Intent(Homepage.this, Olahraga.class);
+        startActivity(b);
+    }
+
+    public void GoToProfile(View view) {
+        Intent b = new Intent(Homepage.this, Profile.class);
         startActivity(b);
     }
 }
--- app/src/main/java/com/akasa/kitafit/activity/Profile.java
+++ app/src/main/java/com/akasa/kitafit/activity/Profile.java
@@ -2,14 +2,20 @@ package com.akasa.kitafit.activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.akasa.kitafit.R;
+import com.akasa.kitafit.adapter.HistoryAdapter;
+import com.akasa.kitafit.model.ProgramKesehatanData;
 import com.akasa.kitafit.model.usermodel;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -21,9 +27,17 @@ import com.google.firebase.database.ValueEventListener;
 import com.mikhaellopez.circularimageview.CircularImageView;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Profile extends AppCompatActivity {
     private FirebaseUser user;
     private FirebaseDatabase firebaseDatabase;
+    RecyclerView mRecyclerView;
+    LayoutInflater inflater;
+    View dialogView;
+    private List<ProgramKesehatanData> listData;
+    private HistoryAdapter adapter;
     String UID;
     TextView mUsername;
     private FirebaseAuth firebaseAuth;
@@ -34,10 +48,18 @@ public class Profile extends AppCompatActivity {
         user = FirebaseAuth.getInstance().getCurrentUser();
         UID=user.getUid();
         final CircularImageView profil = findViewById(R.id.imgprofil_user);
-        firebaseDatabase = FirebaseDatabase.getInstance();
-        DatabaseReference databaseReference = firebaseDatabase.getReference("user");
+        mRecyclerView= findViewById(R.id.recyclerView_history);
         mUsername = findViewById(R.id.userprofil_nama);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setHasFixedSize(true);
+        showListData();
+
         firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
+
+        DatabaseReference databaseReference = firebaseDatabase.getReference("user");
         databaseReference.child(UID).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -51,6 +73,44 @@ public class Profile extends AppCompatActivity {
                 Toast.makeText(Profile.this, databaseError.getCode(), Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void showListData(){
+        DatabaseReference  mRef=FirebaseDatabase.getInstance().getReference("program_kesehatan");
+        final DatabaseReference pkref=FirebaseDatabase.getInstance().getReference("history_program");
+    mRef.addValueEventListener(new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+        if(dataSnapshot.exists()){
+            listData= new ArrayList<>();
+            for(final DataSnapshot ds : dataSnapshot.getChildren()){
+                pkref.child("myRZNQVtrqXe8qH9ML98IvLgd8J3").child("id_program_kesehatan").addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for(DataSnapshot ds2 : dataSnapshot.getChildren()){
+                            if(ds.child("id_program_kesehatan").getValue().equals(ds2.getKey())){
+                                ProgramKesehatanData pk =ds.getValue(ProgramKesehatanData.class);
+                                listData.add(pk);
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+                adapter=new HistoryAdapter(Profile.this,listData);
+            mRecyclerView.setAdapter(adapter);
+        }
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+        }
+    });
     }
 
     public void GoToEdit(View view) {
